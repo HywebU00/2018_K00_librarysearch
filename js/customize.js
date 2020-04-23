8d0326f11f6b1b46a5ef1a21969765274a9509b8
anniewu <annie@hyweb.com.tw>
2020-04-23 17:04:40 +0800

    2020上傳整理

--- js/customize.js
+++ js/customize.js
@@ -1314,3 +1314,44 @@ $(function(){
         $(this).stop().toggleClass('open');
     })
 })
+//閱讀指數－年
+$(function(){
+    $('.linecontent .year-slide').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 980,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        }
+        ]
+    });
+})
