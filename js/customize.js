e6b96ed23aed39f070bc38a55316287f3f89ada0
anniewu <annie@hyweb.com.tw>
2022-04-12 11:19:56 +0800

    20220412新增修改SDG頁面及css、js

--- js/customize.js
+++ js/customize.js
@@ -1053,6 +1053,19 @@ $(function() {
         $('#bank_codestyle').fadeOut();
         $('body').removeClass('fix');
     })
+    // SDG閱讀紀錄
+    $('.sdg_readingbtn').click(function() {
+        $('#sdg_readingstyle').fadeIn();
+        $('body').addClass('fix');
+    })
+    $('.lightboxstyle_block .close').click(function() {
+        $('#sdg_readingstyle').fadeOut();
+        $('body').removeClass('fix');
+    })
+    $('.overlay').click(function() {
+        $('#sdg_readingstyle').fadeOut();
+        $('body').removeClass('fix');
+    })
 })
 //詳目頁 匯出點選
 $(function() {
@@ -1481,6 +1494,7 @@ $(function() {
     $('.addtagbtn').click(function() {
         $('.addtag_block').css('display', 'inline-block');
         $(this).hide();
+        $(this).parents().siblings('ul').find('.addtag_block').css('display', 'none');
     })
     $('.addinput').click(function() {
         $('.addoption').slideDown();
@@ -1673,5 +1687,46 @@ $(function() {
         $(this).parent().parent('tr').next('tr').children('.innertable').slideDown(0);
         $(this).addClass('active');
         $(this).parent().parent('tr').siblings('tr').find('.open_innertable').removeClass('active');
+    })
+})
+// svg
+$(function() {
+    jQuery('img.svg').each(function() {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+})
+// 書房關鍵字
+$(function() {
+    $('.bookdata ul li.keyword').children('a').click(function() {
+        if ($(this).hasClass('graybg')) {
+            $(this).removeClass('graybg');
+            $(this).addClass('greenbg')
+        } else {
+            $(this).removeClass('greenbg');
+            $(this).addClass('graybg')
+        }
     })
 })
